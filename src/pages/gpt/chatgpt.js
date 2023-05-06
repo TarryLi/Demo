45148da79b3e44e7d8b353c305f2f6ff7d733079
litaiyi <litaiyi@jd.com>
2023-05-06 16:11:39 +0800

    feat

--- src/pages/gpt/chatgpt.js
+++ src/pages/gpt/chatgpt.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
-  apiKey: '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'
+  apiKey: 'sk-??'
 });
 
 const openai = new OpenAIApi(configuration);
